ef3d7e9b41daccf1a87b95e9de77b257ac26f748
ABDELAZiZ EL ARASSi <elarassi.abdelaziz@gmail.com>
2021-11-07 23:04:15 +0100

    add button update lang & logout

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
-import {Brand, SafeAreaContainer} from '@components';
+import {Brand, Button, SafeAreaContainer} from '@components';
+import {NavigationService} from '@navigations';
+import {I18n} from '@i18n';
 import styles from './styles';
 
 const Dashboard = props => {
-  // console.log(`props `, props);
+  const [locale, setLocale] = useState('');
+
+  const updateLangue = () => {
+    const newLocale = I18n.currentLocale() === 'fr' ? 'ar' : 'fr';
+    I18n.locale = newLocale;
+    setLocale(newLocale);
+  };
+
+  useEffect(() => {
+    console.log(`I18n.currentLocale() : `, I18n.currentLocale());
+  }, [locale]);
+
   return (
     <SafeAreaContainer scrollEnabled={false}>
       <View style={styles.body}>
         <Brand />
-        <Text style={{marginTop: 40}}>Dashboard Screen</Text>
+        <View>
+          <Text style={styles.label}>{I18n.t('example.title')}</Text>
+          <Text style={styles.label}>
+            Current Locale : {I18n.currentLocale()}
+          </Text>
+          <Text style={styles.title}>Dashboard Screen</Text>
+        </View>
+
+        <Button title="Change Langue" onPress={updateLangue} />
+        <Button
+          style={styles.button}
+          title="LogOut"
+          onPress={() => NavigationService.resetStack('login')}
+        />
       </View>
     </SafeAreaContainer>
   );
--- src/screens/dashboard/styles.js
+++ src/screens/dashboard/styles.js
@@ -1,10 +1,27 @@
 import {StyleSheet} from 'react-native';
+import {Colors} from '@themes';
 
 const styles = StyleSheet.create({
   body: {
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     flex: 1,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: '300',
+    marginTop: 15,
+    marginHorizontal: 70,
+    color: Colors.primary_dark,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '300',
+    marginTop: 15,
+    marginHorizontal: 70,
+  },
+  button: {
+    backgroundColor: Colors.error,
   },
 });
 
