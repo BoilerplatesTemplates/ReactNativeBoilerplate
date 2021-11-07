5c02a1562e5389d68f4effdb7334db53f148f832
ABDELAZiZ EL ARASSi <elarassi.abdelaziz@gmail.com>
2021-11-07 23:12:57 +0100

    example send data with useContext

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import {View, Text} from 'react-native';
 import {Brand, Button, SafeAreaContainer} from '@components';
 import {NavigationService} from '@navigations';
+import {Context as ProfileContext} from '@hooks';
 import {I18n} from '@i18n';
 import styles from './styles';
 
-const Dashboard = props => {
+const Dashboard = () => {
   const [locale, setLocale] = useState('');
+  const {store, setProfile} = useContext(ProfileContext);
+  let IP = store?.profile;
 
   const updateLangue = () => {
     const newLocale = I18n.currentLocale() === 'fr' ? 'ar' : 'fr';
@@ -14,9 +17,12 @@ const Dashboard = props => {
     setLocale(newLocale);
   };
 
-  useEffect(() => {
-    console.log(`I18n.currentLocale() : `, I18n.currentLocale());
-  }, [locale]);
+  useEffect(() => {}, [locale]);
+
+  const logOut = () => {
+    setProfile({profile: null});
+    NavigationService.resetStack('login');
+  };
 
   return (
     <SafeAreaContainer scrollEnabled={false}>
@@ -28,14 +34,11 @@ const Dashboard = props => {
             Current Locale : {I18n.currentLocale()}
           </Text>
           <Text style={styles.title}>Dashboard Screen</Text>
+          <Text style={styles.title}>{`IP : ${IP}`}</Text>
         </View>
 
         <Button title="Change Langue" onPress={updateLangue} />
-        <Button
-          style={styles.button}
-          title="LogOut"
-          onPress={() => NavigationService.resetStack('login')}
-        />
+        <Button style={styles.button} title="LogOut" onPress={logOut} />
       </View>
     </SafeAreaContainer>
   );
--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,23 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {View, Text} from 'react-native';
 import {Brand, Button, SafeAreaContainer} from '@components';
-import {I18n} from '@i18n';
-import {testAPI} from './API';
 import {NavigationService} from '@navigations';
+import {Context as ProfileContext} from '@hooks';
 import {AlertNotifier} from '@helpers';
+import {testAPI} from './API';
+import {I18n} from '@i18n';
 
 import styles from './styles';
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
+  const {setProfile} = useContext(ProfileContext);
 
   const goToDashboard = async () => {
     setLoading(true);
     const res = await testAPI();
     const {success, status, data, error} = res;
-    console.log(`res : `, res);
+
     if (success) {
+      setProfile({profile: data});
       setTimeout(() => NavigationService.resetStack('dashboard'), 600);
       return;
     }
